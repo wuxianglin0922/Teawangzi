4867f9020eaeafd9f46f3fdcee0d544496853b34
lidada12138 <1969553825@qq.com>
2019-04-10 10:50:59 +0800

    etc34

--- MyTeam/app/src/main/java/com/example/lkwan/myteam/ETC36_Change.java
+++ MyTeam/app/src/main/java/com/example/lkwan/myteam/ETC36_Change.java
@@ -1,13 +1,41 @@
 package com.example.lkwan.myteam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class ETC36_Change extends AppCompatActivity {
+    EditText edt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etc36__change);
+        setupview();
     }
+
+    public void doClick(View view) {
+        switch (view.getId()){
+            case R.id.btn1:
+                break;
+            case R.id.btn2:
+                break;
+            case R.id.btn3:
+                break;
+            case R.id.btn4:
+                break;
+        }
+    }
+
+
+
+
+
+    private void setupview() {
+        edt1 = findViewById(R.id.edt1);
+
+    }
+
+
 }
